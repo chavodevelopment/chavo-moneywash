8f5ea03407744794b9368595b7a3a01753ef43e7
chavodevelopment <ysgizax@gmail.com>
2025-02-02 20:51:31 +0300

    Add files via upload

---
+++ chavo-MoneyWash/config.lua
@@ -0,0 +1,8 @@
+
+Config = {}
+
+Config.BlackMoneyItem = 'markedbills'
+Config.WashRate = 1 -- if u type 1 and place 500 markedbilss it will give you 500 normal bill
+Config.Job = "xemlak" -- meslek ismi
+Config.Coords = vector3(-716.41, 260.26, 84.14) -- Menu Location
+
---
+++ chavo-MoneyWash/fxmanifest.lua
@@ -0,0 +1,18 @@
+
+
+fx_version 'cerulean'
+game 'gta5'
+
+author 'Chavo Development'
+description 'chavo-Moneywash'
+
+
+server_scripts { 
+    "server.lua",
+    '@oxmysql/lib/MySQL.lua',
+}
+
+client_scripts { 
+    "client.lua",
+}
+
